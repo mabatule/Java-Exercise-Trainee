6c7028fe6410e2bdb90e89719dd641a16ae978bd
mabatule <maba1998a@gmail.com>
2022-11-11 14:30:28 -0400

    complete exercise 5.7 challenge

--- javaTrainee/src/HighschoolTrainee.java
+++ javaTrainee/src/HighschoolTrainee.java
@@ -3,21 +3,31 @@ import java.util.Scanner;
 public class HighschoolTrainee {
     public static void main(String[] args) {
         //Repeat
-        boolean cancion = true;
+        int numH,earnedMoney,vacationH;
+        double aux,resulttotal;
+        resulttotal=0;
         Scanner rd = new Scanner(System.in);
-        while(cancion){
-            System.out.println("How much is 2x2+2?");
-            System.out.println("1.- 6");
-            System.out.println("2.- 8");
-            System.out.println("3.- 10");
-            int Userinput = rd.nextInt();
-            if (Userinput==1) {
-                cancion = false;
-                System.out.println("Correct!!!");
-            }else{
-                System.out.println("Incorrect!!!");
-            }
 
-        }
+
+        System.out.println("How many hours does the employee work per week?");
+        numH= rd.nextInt();
+
+        System.out.println("How much money does the employee earn per hour?");
+        earnedMoney= rd.nextInt();
+        int result= (numH * earnedMoney) * (12 * 4 );
+        System.out.println("The money earned per year is: "+ result);
+        System.out.println("How many vacation days did you have?");
+        vacationH= rd.nextInt();
+        System.out.println("How many vacation days did you have?");
+        // how much do you earn per day operation
+
+        System.out.println("Taking into account vacation days");
+
+        aux= (result/365) * vacationH;
+        System.out.println("On his vacation days he earned: " + aux);
+        resulttotal = result + aux;
+        System.out.println("Total " + resulttotal);
+
+
     }
 }
