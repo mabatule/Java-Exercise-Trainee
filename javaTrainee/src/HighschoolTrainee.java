0049d4e14f8b945608f200df6e846e3259cb1cb4
mabatule <maba1998a@gmail.com>
2022-11-11 10:39:09 -0400

    complete exercise 2.3

--- javaTrainee/src/HighschoolTrainee.java
+++ javaTrainee/src/HighschoolTrainee.java
@@ -6,10 +6,17 @@ public class HighschoolTrainee {
         char LastInitial = 'S';
         boolean Attendance = true ;
 
+        String StudentName= "Mauricio Balderrama";
+        String FirstName="Mauricio";
+        String LastName="Balderrama";
         System.out.println("The Student Age is "+ StudentAge);
         System.out.println("The Prom is " + Prom);
         System.out.println("The First Initial is " + FirstInitial);
         System.out.println("The last Intial is " + LastInitial);
         System.out.println("The attendance is "+ Attendance);
+        System.out.println("The student name is "+ StudentName);
+        System.out.println("The student First Name " +FirstName);
+        System.out.println("The student Last Name is "+ LastName);
+
     }
 }
