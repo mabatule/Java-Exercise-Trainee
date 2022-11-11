79f667f25a540749a221ff2a8f66b1f3c1bbe9c8
mabatule <maba1998a@gmail.com>
2022-11-11 14:09:22 -0400

    complete exercise 3.6

--- javaTrainee/src/HighschoolTrainee.java
+++ javaTrainee/src/HighschoolTrainee.java
@@ -2,16 +2,22 @@ import java.util.Scanner;
 
 public class HighschoolTrainee {
     public static void main(String[] args) {
-        int Inputname;
-
-        System.out.println("Welcome and thank you for your rating");
+        //Repeat
+        boolean cancion = true;
         Scanner rd = new Scanner(System.in);
-        int score = rd.nextInt();
-        if ( score > 70){
-            System.out.println("Approved");
-        }
-        else {
-           System.out.println("Disapprove");
+        while(cancion){
+            System.out.println("How much is 2x2+2?");
+            System.out.println("1.- 6");
+            System.out.println("2.- 8");
+            System.out.println("3.- 10");
+            int Userinput = rd.nextInt();
+            if (Userinput==1) {
+                cancion = false;
+                System.out.println("Correct!!!");
+            }else{
+                System.out.println("Incorrect!!!");
+            }
+
         }
     }
 }
